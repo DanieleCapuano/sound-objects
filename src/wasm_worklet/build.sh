a6f7b84f45b6e6196046c3aaa4c8cb4ecae36104
DanieleCapuano <el.einad@gmail.com>
2023-09-22 17:07:26 +0200

    optimized emcc build

--- src/wasm_worklet/build.sh
+++ src/wasm_worklet/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-emcc cpp/em_random_gen.cpp -sAUDIO_WORKLET=1 -sWASM_WORKERS=1 -s EXPORTED_RUNTIME_METHODS=["cwrap","setValue","emscriptenRegisterAudioObject"] -o ./em_random_gen.js
+emcc cpp/em_random_gen.cpp -sAUDIO_WORKLET=1 -sWASM_WORKERS=1 -O3 -s EXPORTED_RUNTIME_METHODS=["cwrap","setValue","emscriptenRegisterAudioObject"] -o ./em_random_gen.js
